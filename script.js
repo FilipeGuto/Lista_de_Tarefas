6688670a4d2a514e6cd9c536e7d76f64fe39add8
FilipeGuto <filipeguto95@gmail.com>
2021-06-09 10:38:08 -0300

    Projeto Listas

---
+++ script.js
@@ -0,0 +1,41 @@
+// Ajuda do Baeta para entendimento da logica do exercicio 7;
+// Ajuda do Lucas Caribé na 8 e 10
+function clear() {
+  const lista = document.querySelector('#lista-tarefas');
+  // if (lista) {
+  // for (let index = 0; index < tarefas.length; index += 1) {
+  //   tarefas[index].remove();
+  // }
+
+  // }
+  lista.innerHTML = '';
+}
+function btn() {
+  const botao = document.querySelector('#apaga-tudo');
+  botao.addEventListener('click', clear);
+}
+btn();
+
+function listas(liItem) {
+  liItem.addEventListener('click', (event) => {
+    const evento = event.target;
+    const selectedItem = document.querySelector('.color');
+    if (selectedItem) {
+      selectedItem.classList.remove('color');
+    }
+    evento.classList.add('color');
+  });
+}
+
+function element() {
+  const text = document.querySelector('#texto-tarefa').value;
+  const list = document.querySelector('#lista-tarefas');
+  const newElement = document.createElement('li');
+  listas(newElement);
+  document.querySelector('#texto-tarefa').value = null;
+  newElement.innerText = text;
+  list.appendChild(newElement);
+}
+
+const button = document.querySelector('#criar-tarefa');
+button.addEventListener('click', element);
